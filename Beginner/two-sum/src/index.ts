ffda8777f4149039c37cca1941d9d06d52f24b37
Kevin Kouomeu <kevinkouomeu@icloud.com>
2022-10-15 23:54:53 +0200

    :white_check_mark: Add another test for Two Sum
    Signed-off-by: Kevin Kouomeu <kevinkouomeu@icloud.com>

--- Beginner/two-sum/src/index.ts
+++ Beginner/two-sum/src/index.ts
@@ -1,7 +1,21 @@
 export class TwoSum {
+    getIndice(numbers: number[], target: number): number[] {
+        let arrayOfIndices: number[] = [];
 
-    getIndice(numbers: number[], target: number) {
-        const indice = numbers.indexOf(target);
-        return [indice];
+        if (numbers.length === 2) {
+            arrayOfIndices.push(numbers.indexOf(numbers[0]), numbers.indexOf(numbers[1], 1))
+            return arrayOfIndices;
+        }
+
+        for (let i = 0; i < numbers.length; i++) {
+            for (let j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] === target) {
+                    arrayOfIndices.push(numbers.indexOf(numbers[i]), numbers.indexOf(numbers[j]));
+                    break;
+                }
+            }
+        }
+        return arrayOfIndices;
     }
+
 }
--- Beginner/two-sum/tests/index.spec.ts
+++ Beginner/two-sum/tests/index.spec.ts
@@ -1,43 +1,29 @@
 import {TwoSum} from "../src/index";
 
 let twoSum: TwoSum;
-const array: number[] = [3, 7, 4, 21, 66, 12];
 
 beforeEach(() => {
     twoSum = new TwoSum();
 })
 
 describe('Two Sum Algorithm Leetcode', () => {
-    it('return array of indices of 1 number', () => {
-        expect(twoSum.getIndice(array, 7)).toEqual([1]);
+    it('return array of indices for an array of 2 numbers', () => {
+        const array: number[] = [3, 3];
+        expect(twoSum.getIndice(array, 6)).toEqual([0, 1]);
     });
 
-    it('return array of indices of 1 number such that the number = target', () => {
-        expect(twoSum.getIndice(array, 21)).toEqual([3]);
+    it('return array of indices for an array of 3 numbers', () => {
+        const array: number[] = [3, 2, 4];
+        expect(twoSum.getIndice(array, 6)).toEqual([1, 2]);
+    });
+
+    it('return array of indices for an array of 5 numbers', () => {
+        const array: number[] = [3, 2, 4, 6, 9];
+        expect(twoSum.getIndice(array, 13)).toEqual([2, 4]);
+    });
+
+    it('return array of indices for an array of 10 numbers', () => {
+        const array: number[] = [3, 2, 4, 86, 9, 6, 33, 5, 17, 8];
+        expect(twoSum.getIndice(array, 15)).toEqual([4, 5]);
     });
 });
-
-
-/*
-
-Given an array of integers nums and an integer target, 
-return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, 
-and you may not use the same element twice.
-
-You can return the answer in any order.
-
-Example 
--------
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-
-Scenarios
----------
-- Return array of indices of one number
-- Return array of indices of one number = target
-- Return array of indices of two numbers which sum = target without using the same element twice
-
-*/ 
